e75d08ccb6024bfc9efa11b3f4d654ba2366dd27
sunilreddy.g <sunilsmec@gmail.com>
2026-02-04 22:17:31 +0530

    changed something

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-SG_ID="sg-028ddb0e8c0c3c494" #replace with your ID
-AMI_ID="ami-0220d79f3f480ecf5" # replace with which operating system required
+
+SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z05013202FKF0ZL12WAOP"
+DOMAIN_NAME="daws88s.online"
+
 for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances \
@@ -11,19 +15,22 @@ do
     --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [$instance == "frontend" ]; then
+    if [ $instance == "frontend" ]; then
         IP=$(
-        aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
-        --query 'Reservations[]. Instance[]. PublichIpAddress' \
-        --output text)
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
     else
         IP=$(
-        aws ec2 describe-instances \
-        --instance-ids $INSTANCE_ID \
-        --query 'Reservations[]. Instance[]. PublichIpAddress' \
-        --output text)
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
 
-    echo "IP Adress: $IP"
-done
+    echo "IP Address: $IP"
