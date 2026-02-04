f34271c6d1c624cf765c514030fdf4d234880b15
sunilreddy.g <sunilsmec@gmail.com>
2026-02-04 22:09:57 +0530

    changed something

--- roboshop.sh
+++ roboshop.sh
@@ -3,5 +3,24 @@ SG_ID="sg-028ddb0e8c0c3c494" #replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5" # replace with which operating system required
 for instance in $@
 do
-    aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text
+    instance_id = $( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [$instance == "frontend" ]; then
+        IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[]. Instance[]. PublichIpAddress' \
+        --output text)
+    else
+        IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[]. Instance[]. PublichIpAddress' \
+        --output text)  
 done
